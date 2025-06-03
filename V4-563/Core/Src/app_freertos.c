617e3068d872749f4548d2cafa7bf1343954117b
David Vescovi <dvescovi@tampabay.rr.com>
2025-06-03 13:41:45 -0400

    more os tasks remove warnings

--- V4-563/Core/Src/app_freertos.c
+++ V4-563/Core/Src/app_freertos.c
@@ -51,6 +51,41 @@ const osThreadAttr_t defaultTask_attributes = {
   .priority = (osPriority_t) osPriorityNormal,
   .stack_size = 128 * 4
 };
+/* Definitions for shell */
+osThreadId_t shellHandle;
+const osThreadAttr_t shell_attributes = {
+  .name = "shell",
+  .priority = (osPriority_t) osPriorityLow,
+  .stack_size = 128 * 4
+};
+/* Definitions for script */
+osThreadId_t scriptHandle;
+const osThreadAttr_t script_attributes = {
+  .name = "script",
+  .priority = (osPriority_t) osPriorityLow,
+  .stack_size = 128 * 4
+};
+/* Definitions for led */
+osThreadId_t ledHandle;
+const osThreadAttr_t led_attributes = {
+  .name = "led",
+  .priority = (osPriority_t) osPriorityLow,
+  .stack_size = 128 * 4
+};
+/* Definitions for commandstation */
+osThreadId_t commandstationHandle;
+const osThreadAttr_t commandstation_attributes = {
+  .name = "commandstation",
+  .priority = (osPriority_t) osPriorityNormal,
+  .stack_size = 512 * 4
+};
+/* Definitions for input */
+osThreadId_t inputHandle;
+const osThreadAttr_t input_attributes = {
+  .name = "input",
+  .priority = (osPriority_t) osPriorityAboveNormal,
+  .stack_size = 128 * 4
+};
 
 /* Private function prototypes -----------------------------------------------*/
 /* USER CODE BEGIN FunctionPrototypes */
@@ -85,6 +120,21 @@ void MX_FREERTOS_Init(void) {
   /* creation of defaultTask */
   defaultTaskHandle = osThreadNew(StartDefaultTask, NULL, &defaultTask_attributes);
 
+  /* creation of shell */
+  shellHandle = osThreadNew(ShellTask, NULL, &shell_attributes);
+
+  /* creation of script */
+  scriptHandle = osThreadNew(ScriptTask, NULL, &script_attributes);
+
+  /* creation of led */
+  ledHandle = osThreadNew(LedTask, NULL, &led_attributes);
+
+  /* creation of commandstation */
+  commandstationHandle = osThreadNew(CommandStationTask, NULL, &commandstation_attributes);
+
+  /* creation of input */
+  inputHandle = osThreadNew(InputTask, NULL, &input_attributes);
+
   /* USER CODE BEGIN RTOS_THREADS */
   /* add threads, ... */
   /* USER CODE END RTOS_THREADS */
@@ -104,12 +154,108 @@ void MX_FREERTOS_Init(void) {
 void StartDefaultTask(void *argument)
 {
   /* USER CODE BEGIN defaultTask */
+  (void)argument;
   /* Infinite loop */
   for(;;)
   {
     osDelay(1);
   }
   /* USER CODE END defaultTask */
+}
+
+/* USER CODE BEGIN Header_ShellTask */
+/**
+* @brief Function implementing the shell thread.
+* @param argument: Not used
+* @retval None
+*/
+/* USER CODE END Header_ShellTask */
+void ShellTask(void *argument)
+{
+  /* USER CODE BEGIN shell */
+  (void)argument;
+  /* Infinite loop */
+  for(;;)
+  {
+    osDelay(1);
+  }
+  /* USER CODE END shell */
+}
+
+/* USER CODE BEGIN Header_ScriptTask */
+/**
+* @brief Function implementing the script thread.
+* @param argument: Not used
+* @retval None
+*/
+/* USER CODE END Header_ScriptTask */
+void ScriptTask(void *argument)
+{
+  /* USER CODE BEGIN script */
+  (void)argument;
+  /* Infinite loop */
+  for(;;)
+  {
+    osDelay(1);
+  }
+  /* USER CODE END script */
+}
+
+/* USER CODE BEGIN Header_LedTask */
+/**
+* @brief Function implementing the led thread.
+* @param argument: Not used
+* @retval None
+*/
+/* USER CODE END Header_LedTask */
+void LedTask(void *argument)
+{
+  /* USER CODE BEGIN led */
+  (void)argument;
+  /* Infinite loop */
+  for(;;)
+  {
+    osDelay(1);
+  }
+  /* USER CODE END led */
+}
+
+/* USER CODE BEGIN Header_CommandStationTask */
+/**
+* @brief Function implementing the commandstation thread.
+* @param argument: Not used
+* @retval None
+*/
+/* USER CODE END Header_CommandStationTask */
+void CommandStationTask(void *argument)
+{
+  /* USER CODE BEGIN commandstation */
+  (void)argument;
+  /* Infinite loop */
+  for(;;)
+  {
+    osDelay(1);
+  }
+  /* USER CODE END commandstation */
+}
+
+/* USER CODE BEGIN Header_InputTask */
+/**
+* @brief Function implementing the input thread.
+* @param argument: Not used
+* @retval None
+*/
+/* USER CODE END Header_InputTask */
+void InputTask(void *argument)
+{
+  /* USER CODE BEGIN input */
+  (void)argument;
+  /* Infinite loop */
+  for(;;)
+  {
+    osDelay(1);
+  }
+  /* USER CODE END input */
 }
 
 /* Private application code --------------------------------------------------*/
